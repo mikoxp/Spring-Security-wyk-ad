6b4dfdf0118e48d4729edbd2531be9b2e17ab078
miko <miko>
2015-11-06 11:18:40 +0100

    dodatki

---
+++ dodatkowe/baza.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE account(
+id SERIAL PRIMARY KEY,
+login CHARACTER VARYING(40),
+password CHARACTER VARYING(40)
+);
+CREATE TABLE roles
+(
+  id serial NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+)
+CREATE TABLE account_roles
+(
+  account_id integer NOT NULL,
+  roles_id integer NOT NULL,
+  CONSTRAINT pkey PRIMARY KEY (account_id, roles_id)
+)
